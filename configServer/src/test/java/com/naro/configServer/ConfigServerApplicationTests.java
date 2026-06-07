e70e066c6fb0c646b774d4b660adc30edcbfc365
FrancoArmanasco2000 <programmingarmanasco@gmail.com>
2026-06-07 11:19:11 -0300

    feat: add config service

---
+++ configServer/src/test/java/com/naro/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naro.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
